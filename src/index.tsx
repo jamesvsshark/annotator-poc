5117d41d1aeaa45ece458874c1dab1a55fa805e7
James Simoes <james@pocostud.io>
2022-02-26 21:44:15 -0500

    work

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,24 @@ import TextSelector from 'text-selection-react'
 
 const HEADER_HEIGHT = 300;
 
-const AnnotationForm = () => {
+const AnnotationForm = ({ setShowAnnotationForm, setInCreationMode }) => {
   return (
-    <>d</>
+    <div>
+      <form style={{ display: 'flex', flexDirection: 'column' }}>
+        <textarea style={{ height: '10em' }}></textarea>
+        <div style={{ display: 'flex', marginTop: '.5em' }}>
+          <button type='submit' style={{ marginRight: '.5em' }}>Save</button>
+          <button type='button' onClick={() => {
+            setInCreationMode(false);
+            setShowAnnotationForm(false);
+          }}>Cancel</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
-const AnnotationPopUp = ({ top, selectedRange }) => {
+const AnnotationPopUp = ({ top, selectedRange, setInCreationMode }) => {
   const [showAnnotationForm, setShowAnnotationForm] = React.useState(false)
 
   React.useEffect(() => {
@@ -18,16 +29,19 @@ const AnnotationPopUp = ({ top, selectedRange }) => {
   }, [top])
 
   const handleOnAnnotateClick = () => {
-    var newNode = document.createElement("span");
-    newNode.setAttribute('class', 'to-annotate');
-    selectedRange.surroundContents(newNode);
-    setShowAnnotationForm(true)
+    if (selectedRange) {
+      var newNode = document.createElement("span");
+      newNode.setAttribute('class', 'to-annotate');
+      selectedRange.surroundContents(newNode);
+      setShowAnnotationForm(true);
+      setInCreationMode(true);
+    }
   }
 
   return (
     <>
       <div style={{ top: top, position: 'relative' }}>
-        {showAnnotationForm && <AnnotationForm /> || <>
+        {showAnnotationForm && <AnnotationForm setShowAnnotationForm={setShowAnnotationForm} setInCreationMode={setInCreationMode} /> || <>
           <button onClick={handleOnAnnotateClick}>Annotate this excerpt</button>
         </>}
       </div>
@@ -39,16 +53,16 @@ export const Annotator = ({ text }: { text: any }) => {
   const [topPos, setTopPos] = React.useState(null);
   const [selectedText, setSelectedText] = React.useState(null)
   const [selectedRange, setSelectedRange] = React.useState(null)
+  const [inCreationMode, setInCreationMode] = React.useState(false)
 
   const handleOnMouseup = () => {
-    // console.log(document.getSelection())
-    let selection = document.getSelection();
-    let selRange = selection.getRangeAt(0);
-    let selectedText = selection.toString();
-    let rect = selRange.getBoundingClientRect();
+    if (!inCreationMode) {
+      let selection = document.getSelection();
+      let selRange = selection.getRangeAt(0);
+      let selectedText = selection.toString();
+      let rect = selRange.getBoundingClientRect();
 
-    setSelectedText(selectedText)
-    if (selectedText) {
+      setSelectedText(selectedText)
       setTopPos(rect.top)
       setSelectedRange(selRange)
     }
@@ -56,6 +70,7 @@ export const Annotator = ({ text }: { text: any }) => {
 
   React.useEffect(() => {
     window.addEventListener("mouseup", handleOnMouseup)
+
     return () => {
       window.removeEventListener("mouseup", handleOnMouseup)
     }
@@ -67,7 +82,7 @@ export const Annotator = ({ text }: { text: any }) => {
         <p>{text}</p>
       </div>
       <div style={{ flex: 1 }}>
-        {selectedText && <AnnotationPopUp top={(topPos + window.scrollY) - HEADER_HEIGHT} selectedRange={selectedRange} />}
+        {selectedText && <AnnotationPopUp top={(topPos + window.scrollY) - HEADER_HEIGHT} selectedRange={selectedRange} setInCreationMode={setInCreationMode} />}
       </div>
     </div>
   )
